3f3a5686a84ba4d1cc1b0d95757307fd5c22b73e
Michael Klimonov <pastor.kmc47@gmail.com>
2018-01-13 21:58:12 +0800

    load the latest version

--- app.js
+++ app.js
@@ -14,38 +14,65 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 
 app.use(function (req, res, next) {
-    var origins = [
-        'http://localhost:3000',
-    ];
-
-    for(var i = 0; i < origins.length; i++){
-        var origin = origins[i];
-
-        if(req.headers.origin.indexOf(origin) > -1){
-            res.header('Access-Control-Allow-Origin', req.headers.origin);
-        }
-    }
-    
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  [
+    'http://localhost:3000',
+  ].forEach(origin=>{
+    if (req.headers.origin.indexOf(origin) > -1)
+      res.header('Access-Control-Allow-Origin', req.headers.origin);
+  })
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 
-const { eth } = web3;
+const { eth } = web3; // const eth = web3.eth;
 
 web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
 
-console.log(eth.coinbase);
-
-const transaction = "0x254d9e180c610bafd1b496c3149646b230109826fffd064174c7fe59d1fb66ce";
-
-console.log(eth.getTransaction(transaction));
-console.log(eth.blockNumber);
-
 const helloContract = eth.contract(contract.ABI).at(contract.address);
 
-app.post("/", function(req, res) {
+
+app.post('/add_start_time', (req, res)=>{
+  const { personal } = web3;
+  console.log(req.body);
+  personal.unlockAccount(req.body.account, req.body.password, 1000);
+  res.send(helloContract.setTimeStart.sendTransaction(req.body.firstTime, { from: req.body.account }));
+});
+app.post('/add_end_time', (req, res)=>{
+  const { personal } = web3;
+  console.log(req.body);
+  personal.unlockAccount(req.body.account, req.body.password, 1000);
+  res.send(helloContract.setTimeTo.sendTransaction(req.body.secondTime, { from: req.body.account }));
+});
+
+app.get('/get_contract_data', (req, res) =>{
+  const { personal } = web3;
+  //console.log(req.body);
+  personal.unlockAccount('0xfc25b164424678e293b4d5c451f021b5a03095e2', '123', 1000);
+  //res.send(helloContract.getUser);//докапаться, чтоб сделали транзакцию получения открытого массива 
+  // console.log(helloContract.size().c[0]);
+  var active = 0;
+  var i = 0;
+  var acID = [];
+  while(active < helloContract.size().c[0])
+  {
+    if(!helloContract.keys(i)[1]){
+      acID[active] = helloContract.keys(i)[0];
+      active++;
+    }
+    i++;
+  }
+  response = [];
+  for(var i = 0; i<acID.length; i++){
+    response.push({id: acID[i], unpaidTime: helloContract.exe(acID[i])[2].c[0]});
+  }
+  res.json(response);
+});
+
+app.get("/")
+  
+app.post("/", (req, res)=>{
   const { personal } = web3;
   console.log(req.body);
   personal.unlockAccount(req.body.account, req.body.password, 1000);
@@ -53,6 +80,6 @@ app.post("/", function(req, res) {
   res.send(helloContract.setData.sendTransaction(req.body.message, { from: req.body.account }));
 });
 
-app.listen(8000, function() {
+app.listen(8000, ()=>{
   console.log("Our server on 8000 port");
 });
